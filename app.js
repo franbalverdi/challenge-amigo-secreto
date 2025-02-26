666de42e3b2b0aae558f4a3f1a05e5d576a7b2e5
Franco Balverdi <franconbalverdi@gmail.com>
2025-02-26 18:10:12 -0300

    Actualiza el app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-//Como vemos en el index.html hay dos botones que al hacer click llama a 2 funciones: "agregarAmigo()" y "sortearAmigo()". 
+//Como vemos en el index.html hay dos botones que, al hacer click llama a 2 funciones: "agregarAmigo()" y "sortearAmigo()". 
 //Vamos a crear la función "agregarAmigo()"
 
 let listaAmigos = [];
